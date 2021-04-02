14dcf8dbc1d09f1810a71e5cd3e2669b0c36d005
Ralf Baussenwein <ralf@bwein.net>
2021-04-02 21:39:00 +0200

    Update ecs config

--- ecs.php
+++ ecs.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
-use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
-use SlevomatCodingStandard\Sniffs\TypeHints\DisallowArrayTypeHintSyntaxSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
@@ -18,14 +16,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import($vendorDir.'/contao/easy-coding-standard/config/default.php');
 
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SKIP, [
-        MethodChainingIndentationFixer::class => [
-            '*/DependencyInjection/Configuration.php',
-        ],
-        DisallowArrayTypeHintSyntaxSniff::class => [
-            '*Model.php',
-        ],
-    ]);
 
     $parameters->set(Option::EXCLUDE_PATHS, ['*/templates/*']);
     $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir().'/ecs_default_cache_database_backup');
