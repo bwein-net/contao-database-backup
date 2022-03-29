ec343f42ad043f0de17ed7581c3e57931f0808e6
Ralf Baussenwein <r.baussenwein@eikona-media.de>
2022-03-29 18:08:50 +0200

    Refactor usage of DatabaseBackupDumper to BackupManager

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -12,16 +12,19 @@ declare(strict_types=1);
 
 namespace Bwein\DatabaseBackup\Controller;
 
-use Bwein\DatabaseBackup\Service\DatabaseBackupDumper;
 use Contao\BackendUser;
+use Contao\Config;
+use Contao\CoreBundle\Doctrine\Backup\BackupManager;
 use Contao\CoreBundle\Exception\AccessDeniedException;
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\Message;
 use Contao\System;
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -35,19 +38,21 @@ use Twig\Environment;
  */
 class BackendController extends AbstractController
 {
-    private $router;
-    private $tokenStorage;
-    private $translator;
-    private $twig;
-    private $dumper;
+    private RouterInterface $router;
+    private TokenStorageInterface $tokenStorage;
+    private TranslatorInterface $translator;
+    private Environment $twig;
+    private ContaoFramework $framework;
+    private BackupManager $backupManager;
 
-    public function __construct(RouterInterface $router, TokenStorageInterface $tokenStorage, TranslatorInterface $translator, Environment $twig, DatabaseBackupDumper $dumper)
+    public function __construct(RouterInterface $router, TokenStorageInterface $tokenStorage, TranslatorInterface $translator, Environment $twig, ContaoFramework $framework, BackupManager $backupManager)
     {
         $this->router = $router;
         $this->tokenStorage = $tokenStorage;
         $this->translator = $translator;
         $this->twig = $twig;
-        $this->dumper = $dumper;
+        $this->framework = $framework;
+        $this->backupManager = $backupManager;
     }
 
     public function __invoke(Request $request): Response
@@ -64,43 +69,59 @@ class BackendController extends AbstractController
             throw new AccessDeniedException('Not enough permissions to access database_backup.');
         }
 
+        $this->framework->initialize();
+
         if (!empty($createType = $request->get('create'))) {
             return $this->createAction($createType);
         }
 
         if (!empty($fileName = $request->get('download'))) {
-            return $this->downloadAction($fileName, $request->get('backupType'));
+            return $this->downloadAction($fileName);
         }
 
         return $this->listAction();
     }
 
-    private function createAction(?string $backupType = null): RedirectResponse
+    private function createAction(string $createType = null): RedirectResponse
     {
-        if ('manual' !== $backupType) {
+        if ('manual' !== $createType) {
             Message::addError(
                 $this->translator->trans('database_backup_create_not_allowed')
             );
         }
 
+        $config = $this->backupManager->createCreateConfig();
+
         try {
-            $this->dumper->doBackup($backupType);
+            $this->backupManager->create($config);
             Message::addConfirmation(
                 $this->translator->trans('database_backup_create_successful')
             );
-        } catch (Exception $exception) {
+        } catch (\Exception $exception) {
             Message::addError($this->translator->trans($exception->getMessage()));
         }
 
         return new RedirectResponse($this->router->generate('bwein_contao_database_backup'), 303);
     }
 
-    private function downloadAction(string $fileName, ?string $backupType = null): Response
+    /**
+     * @return StreamedResponse|RedirectResponse
+     */
+    private function downloadAction(string $fileName)
     {
-        if (null !== ($file = $this->dumper->getBackupFile($fileName, $backupType))) {
-            $downloadName = null;
+        $backup = $this->backupManager->getBackupByName($fileName);
 
-            return $this->file($file, $downloadName);
+        if (null !== $backup) {
+            $response = new StreamedResponse(
+                function () use ($backup): void {
+                    $outputStream = fopen('php://output', 'w');
+                    stream_copy_to_stream($this->backupManager->readStream($backup), $outputStream);
+                }
+            );
+            $dispositionHeader = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $fileName);
+            $response->headers->set('Content-Disposition', $dispositionHeader);
+
+            return $response;
         }
 
         Message::addError($this->translator->trans('database_backup_not_found'));
@@ -110,12 +131,37 @@ class BackendController extends AbstractController
 
     private function listAction(): Response
     {
+        System::loadLanguageFile('default');
+        $backups = $this->backupManager->listBackups();
+        $timeZone = new \DateTimeZone(date_default_timezone_get());
+
         $parameters = [
             'backUrl' => System::getReferer(),
             'messages' => Message::generate(),
-            'backups' => $this->dumper->getBackupFilesList(),
+            'backups' => $this->formatForTable($backups, $timeZone),
         ];
 
         return new Response($this->twig->render('@BweinDatabaseBackup/database_backup/index.html.twig', $parameters));
+    }
+
+    private function formatForTable(array $backups, \DateTimeZone $timeZone): array
+    {
+        $formatted = [];
+
+        foreach ($backups as $backup) {
+            // TODO: Change this to \DateTime::createFromInterface($backup->getCreatedAt()) as soon as we require PHP >=8.0
+            $localeDateTime = new \DateTime('@'.$backup->getCreatedAt()->getTimestamp(), $backup->getCreatedAt()->getTimezone());
+            $localeDateTime->setTimezone($timeZone);
+
+            $formatted[] = [
+                'dateTimeRaw' => $backup->getCreatedAt()->getTimestamp(),
+                'dateTime' => $localeDateTime->format(Config::get('datimFormat')),
+                'sizeRaw' => $backup->getSize(),
+                'size' => System::getReadableSize($backup->getSize()),
+                'fileName' => $backup->getFilename(),
+            ];
+        }
+
+        return $formatted;
     }
 }
